58b2adff5336cd8c9f818044763823a4f323ada4
wjd2676 <wjd2676@gmail.com>
2023-09-03 22:49:21 +0900

    Add Introduce team CardList

--- src/pages/TeamMembers/components/Card.tsx
+++ src/pages/TeamMembers/components/Card.tsx
@@ -7,21 +7,56 @@ const Card = (props: CardProps) => {
 
   return (
     <CardWrapper>
-      <div>{name}</div>
-      <div>{mbti}</div>
-      <img src={src} alt={alt} />
-      <div>{position}</div>
-      <div>Env : {env}</div>
-      <div>Tool : {tool}</div>
-      <div>email : {email}</div>
+      <NameContents>{name}</NameContents>
+      <MBTIContents>
+        MBTI :<div>{mbti}</div>
+      </MBTIContents>
+      <ImageContents src={src} alt={alt} />
+      <PositionContents>{position}</PositionContents>
+      <EtcContents>Env : {env}</EtcContents>
+      <EtcContents>Tool : {tool}</EtcContents>
+      <EtcContents>Email : {email}</EtcContents>
     </CardWrapper>
   );
 };
 
 const CardWrapper = styled.div`
-  width: 400px;
-  height: 700px;
+  width: 425px;
   background-color: white;
+  padding: 3rem 15px;
+  border-radius: 15px;
+
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1), 0 6px 20px rgba(0, 0, 0, 0.1);
 `;
+
+const NameContents = styled.div`
+  text-align: left;
+  margin-top: 5px;
+  font-size: 28px;
+  font-weight: 700;
+`;
+
+const MBTIContents = styled(NameContents)`
+  display: flex;
+  flex-direction: row;
+
+  div {
+    color: #4fa986;
+    margin-left: 10px;
+  }
+`;
+
+const ImageContents = styled.img`
+  margin: 3rem 0;
+  width: 200px;
+  height: 200px;
+`;
+
+const PositionContents = styled(NameContents)`
+  font-size: 36px;
+  margin-bottom: 1.5rem;
+`;
+
+const EtcContents = styled(NameContents)``;
 
 export default Card;
